3b3b8478b5ebb9d74c8701c4fc17eba9076d3316
drastiparikh <101419828@georgebrown.ca>
2024-11-22 17:51:07 -0500

    Register Time page created

--- app/src/main/java/com/tndnoob/a3074g22/MainActivity.java
+++ app/src/main/java/com/tndnoob/a3074g22/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tndnoob.a3074g22;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView registerTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        registerTime = findViewById(R.id.clockOutButton);
+        registerTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterTimeDialog dialog = new RegisterTimeDialog();
+                dialog.showDialog(MainActivity.this);
+            }
         });
     }
 }
---
+++ app/src/main/java/com/tndnoob/a3074g22/RegisterTimeActivity.java
@@ -0,0 +1,92 @@
+package com.tndnoob.a3074g22;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class RegisterTimeActivity extends AppCompatActivity {
+
+    TextView clockInButton;
+    TextView clockOutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register_time);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        clockInButton = findViewById(R.id.clockInButton);
+        clockOutButton =findViewById(R.id.clockOutButton);
+
+
+        clockInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showClockInDialog();
+            }
+        });
+        clockOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showClockOutDialog();
+            }
+        });
+    }
+
+    // Method to get the current time in HH:mm format
+    private String getCurrentTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        return sdf.format(new Date());
+    }
+
+    private void showClockInDialog() {
+        String currentTime = getCurrentTime();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Success");
+        builder.setMessage("Successfully Clocked In at " + currentTime);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.create().show();
+
+    }
+    private void showClockOutDialog(){
+        String curTime = getCurrentTime();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Success");
+        builder.setMessage("Successfully Clocked Out at " + curTime);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.create().show();
+
+    }
+}
