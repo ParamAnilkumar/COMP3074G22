aad2aa753b9e921e8bf36bef56256c2d8b77f997
ommakwana <101414422@georgebrown.ca>
2024-11-24 22:06:01 -0500

    Another update made

---
+++ app/src/main/java/com/tndnoob/a3074g22/OrderAdapter.java
@@ -0,0 +1,54 @@
+
+package com.tndnoob.a3074g22;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class OrderAdapter extends BaseAdapter {
+    private Context context;
+    private List<Order> orderList;
+
+    public OrderAdapter(Context context, List<Order> orderList) {
+        this.context = context;
+        this.orderList = orderList;
+    }
+
+    @Override
+    public int getCount() {
+        return orderList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return orderList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item_order, parent, false);
+        }
+
+        Order order = orderList.get(position);
+
+        TextView orderNumberTextView = convertView.findViewById(R.id.orderNumber);
+        TextView orderStatusTextView = convertView.findViewById(R.id.orderStatus);
+
+        orderNumberTextView.setText(order.getOrderNumber());
+        orderStatusTextView.setText(order.getOrderStatus());
+
+        return convertView;
+    }
+}
