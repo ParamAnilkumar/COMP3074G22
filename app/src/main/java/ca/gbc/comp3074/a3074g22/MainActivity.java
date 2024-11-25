7d45a083e6de9b09fd338b024e1d2acc10481967
RallaAnkitha09 <ankithar0107@gmail.com>
2024-11-24 23:48:12 -0500

    Ankitha Ralla

---
+++ app/src/main/java/ca/gbc/comp3074/a3074g22/MainActivity.java
@@ -0,0 +1,62 @@
+package ca.gbc.comp3074.a3074g22;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView adminPortal,POS,registerTime,orderHistory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        adminPortal = findViewById(R.id.textViewAdminPortal);
+        POS = findViewById(R.id.textViewPOS);
+        registerTime = findViewById(R.id.textViewRegisterTime);
+        orderHistory = findViewById(R.id.textViewOrderHistory);
+
+        adminPortal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AdminDialog alert = new AdminDialog();
+                alert.showDialog(MainActivity.this);
+            }
+        });
+        orderHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(i);
+
+            }
+        });
+        registerTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterTimeActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
